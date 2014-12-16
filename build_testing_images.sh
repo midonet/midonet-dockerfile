049524f9a3cde2fc2b594c679a4236c1c33c992f
Antoni Segura Puimedon <toni@midokura.com>
2014-12-16 11:58:39 +0100

    Add centos7 based optional midonet containers
    This patch adds building centos7 images for the midonet specific
    containers and makes it possible to run them alongside the
    ubuntu based cassandra and zookeeper ones.
    
    To try it out:
    
    echo >> fig.yml <<EOF
    zookeeper:
      image: midonet/zookeeper
    cassandra:
      image: midonet/cassandra
    midolman:
      image: midonet/midolman:2014.11-rc3_centos
      links:
      - "zookeeper:zk"
      - "cassandra:cs"
      privileged: true
    api:
      image: midonet/midonet-api:2014.11-rc3_centos
      links:
      - "zookeeper:zk"
      ports:
      - "8080:8080"
    EOF
    
    $ docker run -ti --link midonetdockerfile_api_1:api midonet/midonet-cli:2014.11-rc3_centos midonet-cli
    
    Signed-off-by: Antoni Segura Puimedon <toni@midokura.com>

--- build_testing_images.sh
+++ build_testing_images.sh
@@ -7,11 +7,14 @@ docker build -t midonet/zookeeper dockerfiles/misc/zookeeper
 docker build -t midonet/cassandra:2.1.2 dockerfiles/misc/cassandra
 docker build -t midonet/cassandra dockerfiles/misc/cassandra
 
+docker build -t midonet/midolman:2014.11-rc3_centos dockerfiles/centos/midolman
 docker build -t midonet/midolman:2014.11-rc3_ubuntu dockerfiles/ubuntu/midolman
 docker build -t midonet/midolman dockerfiles/ubuntu/midolman
 
+docker build -t midonet/midonet-api:2014.11-rc3_centos dockerfiles/centos/midonet-api
 docker build -t midonet/midonet-api:2014.11-rc3_ubuntu dockerfiles/ubuntu/midonet-api
 docker build -t midonet/midonet-api dockerfiles/ubuntu/midonet-api
 
+docker build -t midonet/midonet-cli:2014.11-rc3_centos dockerfiles/centos/midonet-cli
 docker build -t midonet/midonet-cli:2014.11-rc3_ubuntu dockerfiles/ubuntu/midonet-cli
 docker build -t midonet/midonet-cli dockerfiles/ubuntu/midonet-cli
