a5b59578733659d0a24a9e7b25d802df658e016e
Carmela Rubinos <carmela.rubinos@gmail.com>
2015-08-17 17:40:08 +0200

    Remove all unused and obsolete dockerfiles
    Deleted all unused and obsolete Dockerfiles at midokura-tools
    
    Change-Id: I6697482abd92663c2fc2c3135557ba9a92ea1d80

--- dockerfiles/ubuntu/midolman/run_midolman.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sed -i '/^servers/c\servers = '$CS_PORT_9042_TCP_ADDR:$CS_PORT_9042_TCP_PORT'' /etc/midolman/midolman.conf
-sed -i '/^zookeeper_hosts/c\zookeeper_hosts = '$ZK_PORT_2181_TCP_ADDR:$ZK_PORT_2181_TCP_PORT'' /etc/midolman/midolman.conf 
-
-exec /usr/share/midolman/midolman-start
